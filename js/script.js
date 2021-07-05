2e3570a5c2ca7f46d27cbae428bd65ba50134b22
Driccioli <driccioli53@gmail.com>
2021-07-05 15:55:29 +0200

    Svolto task base in Vue JS

--- js/script.js
+++ js/script.js
@@ -17,7 +17,56 @@ const data = {
             }, {
                 text: "Mi sono iscritto in palestra!",
                 date: '16-06-2021'
+            },
+            {
+                text: 'Vi presento il mio amico Mimmo',
+                mediaPath: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Supernerd_%283262512306%29.jpg/1024px-Supernerd_%283262512306%29.jpg',
+                date: '17-06-2021'
             }
         ]
     }
 };
+
+var userDetailHtml = document.querySelector(".user-details")
+
+userDetailHtml.innerHTML = `
+    <div class="user-pic"><img src="${data.myProfile.details.pic}" alt="user pic"></div>
+    <div class="user-name">${data.myProfile.details.name} ${data.myProfile.details.surname} </div>
+`
+
+// in vuejs sarà: {{ myProfile.details.name }}
+
+var postListHtml = document.querySelector(".post-list")
+
+data.myProfile.posts.forEach((post) => {
+
+    // prepariamo la parte interna dell'elemento html .post
+    let postHtml = `
+    <div class="post-details"> 
+        <div class="user-pic">
+            <img src="${data.myProfile.details.pic}" alt="user pic">
+        </div>
+        <div class="details">
+            <div class="user-name">${data.myProfile.details.name} ${data.myProfile.details.surname}</div>
+            <div class="post-date">${post.date}</div>
+        </div>
+    </div> 
+    <div class="post-text">
+        ${post.text}
+    </div>
+` 
+    // solo se l'immagine esiste aggiungere a postHtml l'html del media
+    // mediaPath è una chiave che "a volte" esiste. <= verifichiamo che esista.
+    // usiamo Object.keys(post) per ottenere tutte le chiavi di un oggetto => è una lista/array
+    if (Object.keys(post).includes('mediaPath')) {
+        postHtml += ` <div class="post-media">
+        <img src="${post.mediaPath}" alt="media" />
+        </div>`
+    }
+
+    // adesso il nostro html preparato è il postHtml.
+    // dobbiamo inserirlo nel suo container <div class="post"></div>
+    // e il tutto dentro postListHtml.innerHtml.
+
+    postListHtml.innerHTML += `<div class="post"> ${postHtml} </div>`
+}) 
